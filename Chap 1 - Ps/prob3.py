ca518811f06d0af01260ca34a5c9263b10e75924
TAHIR WAQAS <bc240213710twa@vu.edu.pk>
2024-09-08 13:13:19 -0700

    py

--- Chap 1 - Ps/prob3.py
+++ Chap 1 - Ps/prob3.py
@@ -1,3 +1,10 @@
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("I will speak this text")
+# engine.runAndWait()
+
+#Little fun with github copilot 
+
 import pyttsx3
 import datetime
 import random
