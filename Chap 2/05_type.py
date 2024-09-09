787ceb5942bdcfef3c03fe521bc8b2c3f6e2c31d
TAHIR WAQAS <bc240213710twa@vu.edu.pk>
2024-09-09 04:17:32 -0700

    py

---
+++ Chap 2/05_type.py
@@ -0,0 +1,8 @@
+a = "353.6"
+
+# typecasting
+b = float(a)  # a but type shoud be float
+
+t = type(b)
+
+print(t)
---
+++ Chap 2/06_input.py
@@ -0,0 +1,7 @@
+firstNumber = int(input("Enter Your First number: "))
+
+secondNumber = int(input("Enter Your Second number: "))
+
+print("Frist number is: ", firstNumber)
+print("Second number is: ", secondNumber)
+print("Sum is", firstNumber + secondNumber)
