fe5b535c80bd378be5da66461643419f9be68f43
TAHIR WAQAS <bc240213710twa@vu.edu.pk>
2024-09-18 22:03:29 -0700

    py

---
+++ Chap 5 -Ps/prob_1.py
@@ -0,0 +1,9 @@
+words = {
+    "maddad": "Help",
+    "kursi": "Chair",
+    "billi": "Cat",
+}
+
+word = input("Enter the word you want meaning of: ")
+
+print(words[word])
---
+++ Chap 5 -Ps/prob_4.py
@@ -0,0 +1,6 @@
+s = set()
+s.add(18)
+s.add("18")
+s.add(18.0)  # lenght of s afte these operation ?
+
+print(s, len(s))
